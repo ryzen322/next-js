e4e57df72d118b3ee9bb1421d93fad063d0bbead
Rocky Jay <rockyjay_g@yahoo.com>
2024-12-15 02:41:41 -0800

    redeploy

--- components/svg/share.tsx
+++ components/svg/share.tsx
@@ -5,7 +5,10 @@ import Svg from "./svg";
 const Share = () => {
   return (
     <Button className=" flex items-center gap-1">
-      <Svg icon="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+      <Svg
+        className=""
+        icon="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+      />
       <p className=" text-xs font-light text-white/55">1</p>
     </Button>
   );
